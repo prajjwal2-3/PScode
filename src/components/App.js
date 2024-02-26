4357d99669cdd469a2278a59b06e104d88361ee0
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-02-26 19:52:35 +0530

    text-input done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-
-import './App.css';
+import "./App.css";
+import Editor from "./Editor";
 
 function App() {
   return (
-    <div className="App">
-     <div className='editor section'></div>
-     <div className='output'></div>
+    <div className="App flex justify-between bg-slate-500 ">
+      <div className="editor section flex flex-col flex-1 w-6/12 justify-between h-screen ">
+        <Editor displayname="HTML" value="hellow"/>
+        <Editor displayname="CSS"  value="hellow"/>
+        <Editor displayname="JS"  value="hellow"/>
+      </div>
+      <div className="output w-6/12  m-2 rounded-md">
+        <iframe src="" title="output" frameborder="0" sandbox="allow-scripts"
+        width="100%" height="100%"></iframe>
+      </div>
     </div>
   );
 }
---
+++ src/components/Editor.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import { Controlled as ControlledEditor } from 'react-codemirror2'
+function Editor(props) {
+    const {
+        language,
+        displayname,
+        value,
+        onChange
+    }=props
+  return (
+    <div className='bg-slate-700 h-screen  mt-2 mx-2 rounded-md '>
+      <div className='heading bg-white text-black rounded-md p-2'>{displayname}</div>
+      
+        <ControlledEditor
+        className='h-48 overflow-hidden rounded-md'
+        value="oh"
+       options={{
+       theme:'material'
+       }}
+        />
+      
+    </div>
+  )
+}
+
+export default Editor
