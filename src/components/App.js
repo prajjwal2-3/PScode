371cee8703e5376b51492ee2996ef8cf29a03d2d
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-02-26 19:03:18 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className='editor section'></div>
+     <div className='output'></div>
+    </div>
+  );
+}
+
+export default App;
