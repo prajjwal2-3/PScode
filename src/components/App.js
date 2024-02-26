dc7eedee13f38f71072bd5cc05183486c53e9c27
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-02-26 23:16:48 +0530

    string.split problem

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Editor from "./Editor";
 
 function App() {
+const [html,sethtml]=useState('')
+const [css,setcss]=useState('')
+const [js,setjs]=useState('')
+const [src, setsrc]=useState('')
+
+useEffect(()=>{
+const timeout= setTimeout(() => {
+  setsrc(`<html><body>${html}</body><style>${css}</style><script>${js}</script></html>`)
+    
+}, 250);
+console.log(src)
+return()=> clearTimeout(timeout)
+
+
+
+},[html,css,js])
   return (
     <div className="App flex justify-between bg-slate-500 ">
       <div className="editor section flex flex-col flex-1 w-6/12 justify-between h-screen ">
-        <Editor displayname="HTML" value="hellow"/>
-        <Editor displayname="CSS"  value="hellow"/>
-        <Editor displayname="JS"  value="hellow"/>
+        <Editor displayname="HTML" value={html} onChange={sethtml} language="xml"/>
+        <Editor displayname="CSS"  value={css} onChange={setcss} language="CSS"/>
+        <Editor displayname="JS"  value={js} onChange={setjs} language="javascript"/>
       </div>
-      <div className="output w-6/12  m-2 rounded-md">
-        <iframe src="" title="output" frameborder="0" sandbox="allow-scripts"
+      <div className="output w-6/12 bg-white text-black m-2 rounded-md">
+        <iframe srcDoc={src} title="output" frameBorder="0" sandbox="allow-scripts"
         width="100%" height="100%"></iframe>
       </div>
     </div>
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -6,20 +6,30 @@ import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
 import { Controlled as ControlledEditor } from 'react-codemirror2'
 function Editor(props) {
+
+   
     const {
         language,
         displayname,
         value,
-        onChange
-    }=props
+        onChange,
+    }=props;
+
+
+    function handleChange(value){
+        onChange(value)
+    }
+    
   return (
     <div className='bg-slate-700 h-screen  mt-2 mx-2 rounded-md '>
-      <div className='heading bg-white text-black rounded-md p-2'>{displayname}</div>
+      <div className='heading bg-white text-black rounded-md p-2 '>{displayname}</div>
       
         <ControlledEditor
-        className='h-48 overflow-hidden rounded-md code-mirror-wrapper'
-        value="oh"
+        className='h-48 overflow-auto '
+        value={value}
+        onBeforeChange={handleChange}
        options={{
+        lineWrapping: true,
        theme:'material',
        lint:true,
         mode: language,
